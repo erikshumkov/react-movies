49920cedf5119d7de0cec535a27cc2d61affe686
Erik Shumkov <erikshumkov@gmail.com>
2020-10-30 23:50:21 +0100

    Updates

---
+++ precache-manifest.c2408037ce880952fbf59ddf1bb15ad2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef452ede149dfa4167f96a1c4c5624a5",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "5b5e23d422ef8b759260",
+    "url": "/react-movies/static/css/main.5c572d07.chunk.css"
+  },
+  {
+    "revision": "ba3109ec68e63d332298",
+    "url": "/react-movies/static/js/2.62810d39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movies/static/js/2.62810d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b5e23d422ef8b759260",
+    "url": "/react-movies/static/js/main.2e96dd08.chunk.js"
+  },
+  {
+    "revision": "67513ede745015a201e5",
+    "url": "/react-movies/static/js/runtime-main.f2ea0420.js"
+  },
+  {
+    "revision": "8c0efa16fc5596ea430f7b8550749a1f",
+    "url": "/react-movies/static/media/cinema.8c0efa16.jpg"
+  }
+]);
